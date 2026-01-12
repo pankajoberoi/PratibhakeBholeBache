3a82d7e79fc825b37dfa4ddb37ab678680ea1031
pankajoberoi <poberoi.ca19@chitkara.edu.in>
2026-01-12 11:52:25 +0530

    updated

--- Js/DOM.js
+++ Js/DOM.js
@@ -53,18 +53,51 @@
 // let element3 = document.querySelectorAll("p");
 // console.log(element3);
 
-let display = document.querySelector(".screen");
+// let display = document.querySelector(".screen");
 
-let element = document.createElement("h1");
+// let element = document.createElement("h1");
+// let element2 = document.createElement("h2");
+// let element3 = document.createElement("h3");
 
-element.innerText = "I am Dynamic one";
+// element.innerText = "I am Dynamic one";
+// element2.innerHTML = "I am Happy shappy";
+// element3.textContent = "Good Morning mitron!!!";
 
-console.log(element);
+// console.log(element);
 
-display.appendChild(element);
+// display.appendChild(element);
+// display.appendChild(element2);
+
+// display.append(element, element2);
+// display.prepend(element3);
+
+// ================================================================
+
+// let data = Number(prompt("Enter Your salary"));
+// let display = document.querySelector(".screen");
+
+// if (data > 50000) {
+//   let bonus = document.createElement("h1");
+//   bonus.innerText = `You will get 10,000 joining bonus`;
+//   display.appendChild(bonus);
+// } else {
+//   let noBonus = document.createElement("p");
+//   noBonus.innerText = `Bhai mehnat karo`;
+//   display.appendChild(noBonus);
+// }
+
+// login/ Signup
+
+// let orders = ["Laptop", "Phone", "Headphone", "Charger", "Mixy"];
+
+// let display = document.querySelector("#OrderList");
+
+// for (let i = 0; i < orders.length; i++) {
+//   let element = document.createElement("li");
+//   element.innerText = orders[i];
+//   display.append(element);
+// }
 
 // attributes;
 // classlist;
-//prepend
-// append
 //delete
