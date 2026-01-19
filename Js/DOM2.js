008c44e4fd900b4b6e49776e87e1185b22291c4f
pankajoberoi <poberoi.ca19@chitkara.edu.in>
2026-01-19 09:52:29 +0530

    Updated

--- Js/DOM2.js
+++ Js/DOM2.js
@@ -19,9 +19,9 @@
 
 // parent.removeChild(child);
 
-let element = document.getElementById("heading");
+// let element = document.getElementById("heading");
 
-console.log(element.style);
+// console.log(element.style);
 
 // let color = prompt("Enter heading color");
 
@@ -38,16 +38,16 @@ console.log(element.style);
 
 // setAttribute / GetAttribute(event)
 
-let images = [
-  "https://plus.unsplash.com/premium_photo-1767883340045-10f1296830ed?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
-  "https://images.unsplash.com/photo-1765707885402-48f09b9b771e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
-  "https://images.unsplash.com/photo-1765768169488-a5575f71e790?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8",
-];
+// let images = [
+//   "https://plus.unsplash.com/premium_photo-1767883340045-10f1296830ed?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+//   "https://images.unsplash.com/photo-1765707885402-48f09b9b771e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+//   "https://images.unsplash.com/photo-1765768169488-a5575f71e790?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8",
+// ];
 
-let screen = document.querySelector(".displayImages");
+// let screen = document.querySelector(".displayImages");
 
-for (let i = 0; i < images.length; i++) {
-  let image = document.createElement("img");
-  image.setAttribute("src", images[i]);
-  screen.append(image);
-}
+// for (let i = 0; i < images.length; i++) {
+//   let image = document.createElement("img");
+//   image.setAttribute("src", images[i]);
+//   screen.append(image);
+// }
