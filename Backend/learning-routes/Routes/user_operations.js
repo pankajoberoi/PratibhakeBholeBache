8f7d133ca11a6b3f94b53d517939b967f1b89838
pankajoberoi <poberoi.ca19@chitkara.edu.in>
2026-02-21 11:48:47 +0530

    updated

---
+++ Backend/learning-routes/Routes/user_operations.js
@@ -0,0 +1,72 @@
+const express = require("express"); //we have created instance of express
+const router = express.Router();
+
+let studentList = [
+  {
+    id: 1,
+    name: "Aman",
+  },
+  {
+    id: 2,
+    name: "Riya",
+  },
+];
+
+router.post("/signup", (req, res) => {
+  const newStudent = req.body; //frontend
+  let UserName = newStudent.name;
+
+  if (newStudent) {
+    if (UserName.length > 4) {
+      studentList.push(newStudent);
+      res.json({
+        message: "Student Added",
+        studentList,
+      });
+    } else {
+      res.send("Please add a valid name");
+    }
+  } else {
+    res.send("Please add a student");
+  }
+});
+
+router.get("/user", (req, res) => {
+  res.json(studentList);
+});
+
+router.put("/user/:id", (req, res) => {
+  const userId = req.params.id;
+
+  const UpdatedData = req.body;
+
+  studentList = studentList.map((student) =>
+    student.id == userId ? { ...student, UpdatedData } : student,
+  );
+
+  res.json({
+    message: "Student Updated",
+    studentList,
+  });
+});
+
+router.delete("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  console.log(userId);
+
+  studentList = studentList.filter((student) => student.id != userId);
+
+  res.json({
+    message: "Student Deleted",
+    studentList,
+  });
+});
+
+router.get("/", (req, res) => {
+  res.json([
+    { id: 1, role: "Developer" },
+    { id: 2, role: "Tester" },
+  ]);
+});
+
+module.exports = router;
--- Backend/learning-routes/index.js
+++ Backend/learning-routes/index.js
@@ -2,32 +2,11 @@ const express = require("express"); //we have created instance of express
 const app = express();
 const port = 8321;
 
-// CRUD
+const user_operations = require("./Routes/user_operations");
 
-app.get("/", (req, res) => {
-  res.json([
-    { id: 1, role: "Developer" },
-    { id: 2, role: "Tester" },
-  ]);
-});
+app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  res.send("User Signed Up");
-});
-
-app.get("/user", (req, res) => {
-  res.send("User Details");
-});
-
-app.put("/user/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send(`Edit user ${userId}`);
-});
-
-app.delete("/user/:id", (req, res) => {
-  const userId = req.params.id;
-  res.send(`Deleted User ${userId}`);
-});
+app.use("/netflix", user_operations);
 
 app.listen(port, () => {
   console.log("Server running at port " + port);
